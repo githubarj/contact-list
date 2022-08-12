049070fc1be8b3fd96bca66661317cc562b326ee
Richard J Githuba <85483196+githubarj@users.noreply.github.com>
2022-08-12 14:21:11 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -4,73 +4,36 @@ import whiskerson from "./assets/mr-whiskerson.png";
 import felix from "./assets/felix.png";
 import fluffykins from "./assets/fluffykins.png";
 import pumpkin from "./assets/pumpkin.png";
-import phoneIcon from "./assets/phone-icon.png";
-import mailIcon from "./assets/mail-icon.png";
-/* 
-Challenge: Add all the rest of the 
-data to the Contact card instances
-*/
 
 function App() {
   return (
     <div className="contacts">
-      <Contact img={whiskerson} name="Mr. Whiskerson" number="(212) 555-1234"/>
-      <Contact />
-      <Contact />
-      <Contact />
+      <Contact
+        img={whiskerson}
+        name="Mr. Whiskerson"
+        phone="(212) 555-1234"
+        email="mr.whiskaz@catnap.meow"
+      />
+      <Contact
+        img={fluffykins}
+        name="Fluffykins"
+        phone="(212) 555-2345"
+        email="fluff@me.com"
+      />
+      <Contact
+        img={felix}
+        name="Felix"
+        phone="(212) 555-4567"
+        email="thecat@hotmail.com"
+      />
+      <Contact
+        img={pumpkin}
+        name="Pumpkin"
+        phone="(0800) CAT KING"
+        email="pumpkin@scrimba.com"
+      />
     </div>
   );
 }
-// <div className="contact-card">
-//     <img src="./images/mr-whiskerson.png"/>
-//     <h3>Mr. Whiskerson</h3>
-//     <div className="info-group">
-//         <img src="./images/phone-icon.png" />
-//         <p>(212) 555-1234</p>
-//     </div>
-//     <div className="info-group">
-//         <img src="./images/mail-icon.png" />
-//         <p>mr.whiskaz@catnap.meow</p>
-//     </div>
-// </div>
-
-// <div className="contact-card">
-//     <img src="./images/fluffykins.png"/>
-//     <h3>Fluffykins</h3>
-//     <div className="info-group">
-//         <img src="./images/phone-icon.png" />
-//         <p>(212) 555-2345</p>
-//     </div>
-//     <div className="info-group">
-//         <img src="./images/mail-icon.png" />
-//         <p>fluff@me.com</p>
-//     </div>
-// </div>
-
-// <div className="contact-card">
-//     <img src="./images/felix.png"/>
-//     <h3>Felix</h3>
-//     <div className="info-group">
-//         <img src="./images/phone-icon.png" />
-//         <p>(212) 555-4567</p>
-//     </div>
-//     <div className="info-group">
-//         <img src="./images/mail-icon.png" />
-//         <p>thecat@hotmail.com</p>
-//     </div>
-// </div>
-
-// <div className="contact-card">
-//     <img src="./images/pumpkin.png"/>
-//     <h3>Pumpkin</h3>
-//     <div className="info-group">
-//         <img src="./images/phone-icon.png" />
-//         <p>(0800) CAT KING</p>
-//     </div>
-//     <div className="info-group">
-//         <img src="./images/mail-icon.png" />
-//         <p>pumpkin@scrimba.com</p>
-//     </div>
-// </div>
 
 export default App;
--- src/Contact.jsx
+++ src/Contact.jsx
@@ -3,18 +3,19 @@ import whiskerson from "./assets/mr-whiskerson.png"
 import phoneIcon from "./assets/phone-icon.png"
 import mailIcon from "./assets/mail-icon.png"
 
-export default function Contact() {
+export default function Contact(props) {
+  const { img, name , phone, email } = props
   return (
     <div className="contact-card">
-      <img src={whiskerson} />
-      <h3>Mr. Whiskerson</h3>
+      <img src={img} />
+      <h3>{name}</h3>
       <div className="info-group">
         <img src={phoneIcon} />
-        <p>(212) 555-1234</p>
+        <p>{phone}</p>
       </div>
       <div className="info-group">
         <img src={mailIcon} />
-        <p>mr.whiskaz@catnap.meow</p>
+        <p>{email}</p>
       </div>
     </div>
   );
